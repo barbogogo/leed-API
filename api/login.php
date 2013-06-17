edcc6b95a55df1ea2b5a15d036abeb75e6263ab4
Barbogogo <barbogogo@barbogogo.fr>
2013-06-17 13:54:54 +0200

    Correction : Retourner une erreur si l'API est désactivée
    Mise à jour suite au ticket #7

--- api/login.php
+++ api/login.php
@@ -7,18 +7,26 @@ require_once('./auth.php');
 
 $realm = $_SERVER['SERVER_NAME'];
 
-if(!$myUser && !($myUser = auth_check($realm))) 
+if(PLUGIN_ENABLED == 1)
 {
-   auth_request($realm);
+    if(!$myUser && !($myUser = auth_check($realm))) 
+    {
+       auth_request($realm);
+    }
+    else
+    {
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Expires:'.gmdate('D, d M Y H:i:s \G\M\T', time() + 3600));
+        header('Content-type: application/json');
+        echo "{\"error\":{\"id\":\"0\",\"message\":\"no error\"}}\n";
+    }
+
+    $_SESSION['currentUser'] = serialize($myUser);
 }
 else
 {
-    header('Cache-Control: no-cache, must-revalidate');
-    header('Expires:'.gmdate('D, d M Y H:i:s \G\M\T', time() + 3600));
-    header('Content-type: application/json');
-    echo "{\"error\":{\"id\":\"0\",\"message\":\"no error\"}}\n";
+    // Error#1: plugin disable
+    echo "{\"error\":{\"id\":\"1\",\"message\":\"API disabled\"}}\n";
 }
 
-$_SESSION['currentUser'] = serialize($myUser);
- 
 ?>
