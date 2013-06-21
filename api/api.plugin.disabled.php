933c9109d3d868b99818f111b12a109cb8fcd9c5
Barbogogo <barbogogo@barbogogo.fr>
2013-06-21 13:17:30 +0200

    Changement du lien internet

--- api/api.plugin.disabled.php
+++ api/api.plugin.disabled.php
@@ -2,7 +2,7 @@
 /*
 @name api
 @author Barbogogo <barbogogo@barbogogo.fr>
-@link http://projets.barbogogo.fr
+@link http://www.barbogogo.fr/projets/applications-leed/leed-api/
 @licence CC by nc sa <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/fr/">http://creativecommons.org/licenses/by-nc-sa/2.0/fr/</a>
 // @version 0.3
 @description Le plugin API permet de gérer ses flux Leed via une application externe
